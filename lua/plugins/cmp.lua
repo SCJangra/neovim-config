0db7563c83340e79e6ad4ecdb5e99a7e3b64bda0
Sachin Charakhwal <sachincharakhwal@gmail.com>
2023-07-25 12:51:46 +0530

    plugin(nvim-cmp): add kind icons

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,3 +1,27 @@
+local options = {
+  snippet = {
+    expand = function(args) require('luasnip').lsp_expand(args.body) end,
+  },
+  window = {
+    completion = {
+      winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+      col_offset = -3,
+      side_padding = 0,
+    },
+  },
+  formatting = {
+    fields = { 'kind', 'abbr', 'menu' },
+    format = function(entry, vim_item)
+      local kind = require('lspkind').cmp_format({ mode = 'symbol_text', maxwidth = 50 })(entry, vim_item)
+      local strings = vim.split(kind.kind, '%s', { trimempty = true })
+      kind.kind = ' ' .. (strings[1] or '') .. ' '
+      kind.menu = '    [' .. (strings[2] or '') .. ']'
+
+      return kind
+    end,
+  }
+}
+
 return {
   'hrsh7th/nvim-cmp',
   dependencies = {
@@ -6,8 +30,10 @@ return {
     'hrsh7th/cmp-path',
     'hrsh7th/cmp-cmdline',
     'saadparwaiz1/cmp_luasnip',
-    'L3MON4D3/LuaSnip'
+    'L3MON4D3/LuaSnip',
+    'onsails/lspkind.nvim'
   },
+  opts = options,
   config = function(_, opts)
     local cmp = require 'cmp'
 
@@ -19,9 +45,4 @@ return {
 
     cmp.setup(opts)
   end,
-  opt = {
-    snippet = {
-      expand = function(args) require('luasnip').lsp_expand(args.body) end,
-    }
-  }
 }
