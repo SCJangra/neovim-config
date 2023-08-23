fec6eebf432f90b8ec4e025b4ddb7fb927892516
Sachin Charakhwal <sachincharakhwal@gmail.com>
2023-08-23 20:24:43 +0530

    feat: add gitsigns

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,21 @@
+local opts = {
+  signs                   = {
+    add          = { text = '▌' },
+    change       = { text = '▌' },
+    delete       = { text = '_' },
+    topdelete    = { text = '‾' },
+    changedelete = { text = '~' },
+    untracked    = { text = '🮌' },
+  },
+  word_diff               = true,
+  current_line_blame      = true,
+  current_line_blame_opts = {
+    delay = 100,
+  }
+}
+
+return {
+  'lewis6991/gitsigns.nvim',
+  event = 'BufReadPost',
+  opts = opts,
+}
