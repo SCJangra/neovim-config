8b34fc255a9fc782a7294e33f54afabd04d0df50
Sachin Charakhwal <sachincharakhwal@gmail.com>
2023-08-25 21:55:47 +0530

    change fold column width

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -30,11 +30,11 @@ local keys = {
 
 local init = function()
   local o = vim.o
-  o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
+  o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
   o.foldlevelstart = 99
   o.foldenable = true
   o.fillchars = [[eob: ,fold: ,foldopen:󰧗,foldsep: ,foldclose:󰧛]]
-  o.foldcolumn = '0'   -- '0' is not bad
+  o.foldcolumn = '1' -- '0' is not bad
 end
 
 vim.api.nvim_create_autocmd('BufEnter', { command = 'UfoAttach' })
