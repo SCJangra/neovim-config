4f68638664579f6f2321fb3d0307aead85f06e03
Sachin Charakhwal <sachincharakhwal@gmail.com>
2023-08-28 09:33:00 +0530

    lspconfig: set keymsps on attach

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,19 +3,22 @@ local diagnostic = vim.diagnostic
 local buf = vim.lsp.buf
 local utils = require('utils')
 
-local generic_setup = function()
-  set('n', 'gD', buf.declaration, { desc = 'LSP declaration' })
-  set('n', 'K', buf.hover, { desc = 'LSP hover' })
-  set('n', '<leader>lr', buf.rename, { desc = 'Rename' })
-  set('n', '<leader>la', buf.code_action, { desc = 'Code action' })
-  set('n', '<leader>ll', diagnostic.open_float, { desc = 'Show line diagnostic' })
-  set('n', '<leader>lj', diagnostic.goto_next, { desc = 'Next diagnostic' })
-  set('n', '<leader>lk', diagnostic.goto_next, { desc = 'Prev diagnostic' })
-  set('n', '<leader>lR', '<CMD>LspRestart<CR>', { desc = 'Restart' })
+local setup_keymspa = function(bufnr)
+  set('n', 'gD', buf.declaration, { buffer = bufnr, desc = 'LSP declaration' })
+  set('n', 'K', buf.hover, { buffer = bufnr, desc = 'LSP hover' })
+  set('n', '<leader>lr', buf.rename, { buffer = bufnr, desc = 'Rename' })
+  set('n', '<leader>la', buf.code_action, { buffer = bufnr, desc = 'Code action' })
+  set('n', '<leader>ll', diagnostic.open_float, { buffer = bufnr, desc = 'Show line diagnostic' })
+  set('n', '<leader>lj', diagnostic.goto_next, { buffer = bufnr, desc = 'Next diagnostic' })
+  set('n', '<leader>lk', diagnostic.goto_next, { buffer = bufnr, desc = 'Prev diagnostic' })
+  set('n', '<leader>lR', '<CMD>LspRestart<CR>', { buffer = bufnr, desc = 'Restart' })
+end
 
+local generic_setup = function()
   return {
     capabilities = require('cmp_nvim_lsp').default_capabilities(),
     on_attach = function(client, bufnr)
+      setup_keymspa(bufnr)
       utils.setup_auto_format(client, bufnr)
       require('lsp_signature').on_attach(nil, bufnr)
     end
