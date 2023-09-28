8eeb8447fbe39f4423b9a6e0a938d3e62e91aaaf
Sachin Charakhwal <sachincharakhwal@gmail.com>
2023-09-28 19:03:58 +0530

    fix: typo

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -25,7 +25,7 @@ local opts = {
 
 local keys = {
   { 'zR', function() require('ufo').openAllFolds() end,  desc = 'Open all folds' },
-  { 'zM', function() require('ufo').closeAllFolds() end, desc = 'Open all folds' },
+  { 'zM', function() require('ufo').closeAllFolds() end, desc = 'Close all folds' },
 }
 
 local init = function()
