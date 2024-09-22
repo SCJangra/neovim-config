170138288f8da88b2f0601de9db4678aeca26136
Sachin Charakhwal <sachincharakhwal@gmail.com>
2024-09-22 22:49:14 +0530

    disable spell check in terminal

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -2,3 +2,8 @@ vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
   pattern = "*.wgsl",
   callback = function() vim.bo.filetype = "wgsl" end,
 })
+
+vim.api.nvim_create_autocmd('TermOpen', {
+  pattern = '*',
+  command = 'setlocal nospell',
+})
