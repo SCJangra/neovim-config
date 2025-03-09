53f02a2997295be64f489060bcb65c1cdeacc2ba
Sachin Charakhwal <sachincharakhwal@gmail.com>
2025-03-09 11:18:24 +0530

    update `crates.nvim` config

--- lua/plugins/crates.lua
+++ lua/plugins/crates.lua
@@ -2,5 +2,14 @@ return {
   'saecki/crates.nvim',
   tag = 'stable',
   event = 'BufRead Cargo.toml',
-  opts = {},
+  opts = {
+    thousands_separator = ",",
+    lsp = {
+      enabled = true,
+      actions = true,
+      completion = true,
+      hover = true,
+      on_attach = function(_, bufnr) require('utils.lsp').setup_keymspa(bufnr) end
+    }
+  },
 }
