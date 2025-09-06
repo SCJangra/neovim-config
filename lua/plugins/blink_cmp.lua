d73f604deb01e054a4d1a36b0cd1cf1860d5bcb6
Sachin Charakhwal <sachincharakhwal@gmail.com>
2025-09-06 18:53:48 +0530

    blink_cmp: disable cmdline integration

--- lua/plugins/blink_cmp.lua
+++ lua/plugins/blink_cmp.lua
@@ -20,6 +20,8 @@ return {
     -- See :h blink-cmp-config-keymap for defining your own keymap
     keymap = { preset = 'enter' },
 
+    cmdline = { enabled = false },
+
     appearance = {
       -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
       -- Adjusts spacing to ensure icons are aligned
