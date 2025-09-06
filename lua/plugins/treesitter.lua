58fd36a61c4e9fc7f42abc1ef0797879ecd49dc6
Sachin Charakhwal <sachincharakhwal@gmail.com>
2025-09-06 18:53:34 +0530

    treesitter: disable installing `ipkg`

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,10 +2,13 @@ return {
   'nvim-treesitter/nvim-treesitter',
   event = 'BufReadPost',
   config = function()
+    ---@diagnostic disable-next-line: missing-fields
     require('nvim-treesitter.configs').setup {
       ensure_installed = 'all',
       highlight = { enable = true },
       indent = { enable = true },
+      -- These are having difficulty installing, so I've temporarily disabled them.
+      ignore_install = { 'ipkg' },
     }
   end
 }
