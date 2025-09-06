523160ac9c83184b30b406a79ed119b6d584b06a
Sachin Charakhwal <sachincharakhwal@gmail.com>
2025-09-06 17:42:01 +0530

    noice: comment out lsp related config

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,11 +1,11 @@
 local opts = {
-  lsp = {
-    override = {
-      ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-      ['vim.lsp.util.stylize_markdown'] = true,
-      ['cmp.entry.get_documentation'] = true,
-    },
-  },
+  -- lsp = {
+  --   override = {
+  --     ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+  --     ['vim.lsp.util.stylize_markdown'] = true,
+  --     ['cmp.entry.get_documentation'] = true,
+  --   },
+  -- },
   views = {
     cmdline_popup = {
       position = { row = 5, col = '50%' },
