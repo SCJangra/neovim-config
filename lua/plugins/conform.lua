2dc4348286cd963d5a68d2255c66a816db599abe
Sachin Charakhwal <sachincharakhwal@gmail.com>
2025-11-07 12:37:08 +0530

    do not auto format nix

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -11,7 +11,13 @@ return {
 
     vim.api.nvim_create_autocmd('BufWritePre', {
       pattern = '*',
-      callback = function(args) require('conform').format { bufnr = args.buf, lsp_fallback = true } end,
+      callback = function(args)
+        local ft = vim.bo[args.buf].filetype
+        local disable = { nix = true }
+        if disable[ft] then return end
+
+        require('conform').format { bufnr = args.buf, lsp_fallback = true }
+      end,
     })
 
     require('conform').setup(options)
