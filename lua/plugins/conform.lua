85f4cddd8a03f4843b6c34a48b430e52640982b7
Sachin Charakhwal <sachincharakhwal@gmail.com>
2026-02-16 19:58:53 +0530

    plugin(conform): format `javascript` and `typrscript` using `prettier`

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -6,6 +6,8 @@ return {
       formatters_by_ft = {
         lua = { 'stylua' },
         rust = { 'rustfmt' },
+        javascript = { 'prettier' },
+        typescript = { 'prettier' },
       },
     }
 
