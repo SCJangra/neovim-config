3e9d9e2e954098a20542018c118a664c623db950
Sachin Charakhwal <sachincharakhwal@gmail.com>
2026-03-01 23:46:26 +0530

    chore: update plugins

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs { inherit system; };
     in
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   event = 'BufReadPost',
+  tag = 'v0.10.0',
   config = function()
     ---@diagnostic disable-next-line: missing-fields
     require('nvim-treesitter.configs').setup {
